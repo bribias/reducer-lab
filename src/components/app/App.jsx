4ad9b48c19822fa8c706b42874e48f012367da67
Brianna Bias <brianna.bias@gmail.com>
2021-09-30 14:28:17 -0700

    init value and color history written

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,34 +1,21 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 
-const useRecord = (init) => {
-  const [before, setBefore] = useState([]);
-  const [current, setCurrent] = useState(init);
-  const [after, setAfter] = useState([]);
-
-  const undo = () => {
-    setAfter(after => [current, ...after]);
-    setCurrent(before[before.length - 1]);
-    setBefore(before => before.slice(0, -1));
-  };
-
-  const redo = () => {
-    setBefore(before => [...before, current]);
-    setCurrent(after[0]);
-    setAfter(after => after.slice(1));
-  }
-
-  const record = val => {
-    setBefore(before => [...before, current]);
-    setCurrent(val);
-  }
-
-  return {
-    undo,
-    record,
-    redo,
-    current,
-  }
+const initialValue = {
+  before: [],
+  current: '#FF0000',
+  after: ['#0000FF']
 };
+
+const colorHistory = (state, action) => {
+  switch (action.type) {
+    case 'undo':
+      return {
+        after: [state.current, ...state.after],
+        current: state.before[state.before.length - 1],
+        before: state.before.slice(0, -1)
+      };
+  }
+}
 
 function App() {
   const { current, undo, redo, record } = useRecord('#FF0000');
@@ -38,7 +25,7 @@ function App() {
       <button onClick={undo}>undo</button>
       <button onClick={redo}>redo</button>
       <input type="color" value={current} onChange={({ target }) => record(target.value)} />
-      <div style=></div>
+      <div></div>
     </>
   )
 }
