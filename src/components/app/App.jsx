223fcbd17fad4a0228b710a484f5e120e7c89e82
Brianna Bias <brianna.bias@gmail.com>
2021-09-30 14:35:22 -0700

    color history finished

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -14,8 +14,22 @@ const colorHistory = (state, action) => {
         current: state.before[state.before.length - 1],
         before: state.before.slice(0, -1)
       };
+    case 'redo':
+      return {
+        before: [...state.before, state],
+        current: state.after[0],
+        after: state.after.slice(1)
+      };
+    case 'record':
+      return {
+        ...state,
+        before: [...state.before, state.current],
+        current: action.payload
+      };
+    default:
+      return new Error(`Invalid action, unexpected type: ${action.type}`);
   }
-}
+};
 
 function App() {
   const { current, undo, redo, record } = useRecord('#FF0000');
